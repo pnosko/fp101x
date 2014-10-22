c8b1ed4cdcdfe0073b8a3578449c9d8595faf571
ThatScienceGeek <peter.nosko@gmail.com>
2014-10-21 18:36:11 -0700

    Initial exercise

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,11 @@
 -- | Main entry point to the application.
 module Main where
 
+-- last [x] = x
+-- last xs = head drop length xs xs
+
 -- | The main entry point.
 main :: IO ()
 main = do
     putStrLn "Welcome to FP Haskell Center!"
     putStrLn "Have a good day!"
-    
-    putStrLn head drop length xs xs
-        where xs = [1..5]
